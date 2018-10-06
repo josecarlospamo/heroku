0a168f03ed1f5a2cabaaaa41543b414247329f7d
josecarlospamo <josecarlospamo@gmail.com>
2018-10-06 04:56:57 -0500

    primer commit

---
+++ datos/configuracion.php
@@ -0,0 +1,12 @@
+<?php
+
+// Notificar todos los errores excepto E_NOTICE
+error_reporting(0);
+
+define("SERVIDOR_BD", "ec2-23-21-121-220.compute-1.amazonaws.com");
+define("USUARIO_BD", "zuliyqprtzfdal");
+define("CLAVE_BD", "80a16cdac59930690b701757e49665dd0259fc3d15de8b80b9cdb3d157df3fc8");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "dc811qhlus5erf");
+
+ 
